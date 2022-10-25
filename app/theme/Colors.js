7a09e04c978dcb2a01707fc0b6206512b3583192
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2022-10-25 20:36:23 +0530

    Updated Colors and Images constants

---
+++ app/theme/Colors.js
@@ -0,0 +1,20 @@
+const Colors = {
+  Background: {
+    PRIMARY_COLOR: '#EAE9E9',
+    FULL_WHITE: '#FFFFFF',
+  },
+  Text: {
+    PRIMARY_COLOR: '#000000',
+    SECONDARY_COLOR: '#FFFFFF',
+    LINKS: '#0F00BE',
+  },
+  TextField: {
+    BACKGROUND_PRIMARY_COLOR: '#D9D9D9',
+  },
+  Button: {
+    PRIMARY_COLOR: '#190662',
+    DISABLED_COLOR: '#A9A9A9',
+  },
+};
+
+export {Colors};
--- app/theme/Images.js
+++ app/theme/Images.js
@@ -5,10 +5,13 @@ const Images = {
   //     screen_03: require('../assets/Images/Onboarding/Screen03/screen_03.png'),
   //   },
   backgrounds: {
-    login: require('../assets/Images/login.png'),
-    otp: require('../assets/Images/login.png'),
-    forgotPassword: require('../assets/Images/login.png'),
-    resetPassword: require('../assets/Images/login.png'),
+    login: require('../assets/Images/Backgrounds/login2x.png'),
+    // otp: require('../assets/Images/login.png'),
+    // forgotPassword: require('../assets/Images/login.png'),
+    // resetPassword: require('../assets/Images/login.png'),
+  },
+  icons: {
+    google: require('../assets/Images/Icons/google.png'),
   },
 };
 
